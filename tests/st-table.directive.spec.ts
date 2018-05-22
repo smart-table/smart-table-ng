4a0cf41cda1b750941891939189bfbad88bd152e
lorenzofox3 <laurent34azerty@gmail.com>
2018-05-22 20:29:24 +0200

    add more test

--- tests/st-table.directive.spec.ts
+++ tests/st-table.directive.spec.ts
@@ -30,6 +30,7 @@ const fixtureData: User[] = [
     {name: 'Raymond', age: 35, email: 'nooo@bexample.com'}
 ];
 
+
 describe('StTable directive', () => {
     let stInstance: SmartTable<User>;
     const createComponent = (template = `<ul stTable #list="stTable">
@@ -163,4 +164,40 @@ describe('StTable directive', () => {
             done();
         }, 45);
     });
+
+    it('should re render the items when service emit display change event', done => {
+        const fixture = createComponent();
+        stInstance.sort({pointer: 'name'});
+
+        setTimeout(() => {
+            fixture.detectChanges();
+            const el: HTMLElement = fixture.nativeElement;
+            const items = Array.from(el.querySelectorAll('li'));
+            expect(items.length).toBe(3);
+            expect(items.map(i => i.textContent.trim()))
+                .toEqual([
+                    'Albert',
+                    'Bob',
+                    'Raymond'
+                ]);
+            done();
+        }, 45);
+    });
+
+    it('should emit display event', done => {
+        const fixture = createComponent(`<ul stTable (display)="handleEvent($event)"></ul>`);
+        stInstance.sort({pointer: 'name'});
+        setTimeout(() => {
+            fixture.detectChanges();
+            const component = fixture.componentInstance;
+            expect(component.args).toEqual([
+                {index: 1, value: {name: 'Albert', age: 40, email: 'woo@example.com'}},
+                {index: 0, value: {name: 'Bob', age: 30, email: 'foo@bar.com'}},
+                {index: 2, value: {name: 'Raymond', age: 35, email: 'nooo@bexample.com'}}
+            ]);
+
+            done();
+        }, 45);
+    });
+
 });
